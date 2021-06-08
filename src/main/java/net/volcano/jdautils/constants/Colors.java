6ad81b38b71315fe15d3974c122a1486b6e6e083
VolcanoCookies <franek.kg@gmail.com>
2021-06-08 12:53:37 +0200

    Added new color

--- src/main/java/net/volcano/jdautils/constants/Colors.java
+++ src/main/java/net/volcano/jdautils/constants/Colors.java
@@ -7,6 +7,7 @@ public class Colors {
 	public static final Color ERROR = new Color(220, 100, 100);
 	public static final Color SUCCESS = new Color(100, 220, 100);
 	public static final Color INFO = new Color(100, 100, 220);
+	public static final Color PARTIAL_SUCCESS = new Color(255, 170, 0);
 	
 	public static final Color HELP = new Color(255, 204, 0);
 	public static final Color NOTES = new Color(0, 150, 255);
