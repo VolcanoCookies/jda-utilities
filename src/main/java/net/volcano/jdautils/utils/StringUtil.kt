0daa95c5065eaebcb492bc181394285569cc75fe
VolcanoCookies <franek.kg@gmail.com>
2021-06-10 17:15:43 +0200

    Added tokenize method to extract tokens from strings, while respecting quotes

--- src/main/java/net/volcano/jdautils/utils/StringUtil.kt
+++ src/main/java/net/volcano/jdautils/utils/StringUtil.kt
@@ -1,11 +1,7 @@
-package net.volcano.jdautils.utils;
+package net.volcano.jdautils.utils
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+object StringUtil {
 
-public class StringUtil {
-	
 	/**
 	 * Split a string into multiple strings, trying to split at the provided string
 	 * If content contains any substring longer than maxLength and not containing split,
@@ -16,54 +12,66 @@ public class StringUtil {
 	 * @param split     The string to try and split at
 	 * @return A list of strings in order
 	 */
-	public static List<String> splitAt(String content, int maxLength, String split) {
-		List<String> strings = new ArrayList<>();
-		
-		StringBuilder stringBuilder = new StringBuilder();
-		
-		if (content.length() < maxLength) {
-			return Collections.singletonList(content);
+	fun splitAt(content: String, maxLength: Int, split: String): List<String> {
+		val strings: MutableList<String> = ArrayList()
+		var stringBuilder = StringBuilder()
+		if (content.length < maxLength) {
+			return listOf(content)
 		}
-		
-		for (String s : content.split(split)) {
-			
-			while (s.length() > maxLength) {
-				stringBuilder.append(s, 0, maxLength);
-				s = s.substring(maxLength);
+		for (str in content.split(split.toRegex()).toTypedArray()) {
+			var s = str
+			while (s.length > maxLength) {
+				stringBuilder.append(s, 0, maxLength)
+				s = s.substring(maxLength)
 			}
-			
-			if (s.length() + stringBuilder.length() > maxLength) {
-				strings.add(stringBuilder.toString());
-				stringBuilder = new StringBuilder();
+			if (s.length + stringBuilder.length > maxLength) {
+				strings.add(stringBuilder.toString())
+				stringBuilder = StringBuilder()
 			}
-			
-			stringBuilder.append(s);
-			
+			stringBuilder.append(s)
 		}
-		
-		if (stringBuilder.length() > 0) {
-			strings.add(stringBuilder.toString());
+		if (stringBuilder.length > 0) {
+			strings.add(stringBuilder.toString())
 		}
-		
-		return strings;
+		return strings
 	}
-	
-	public static String trim(String content, int maxLength) {
-		return content.length() > maxLength ? content.substring(0, maxLength - 1) : content;
+
+	fun trim(content: String, maxLength: Int): String {
+		return if (content.length > maxLength) content.substring(0, maxLength - 1) else content
 	}
-	
-	public static String capitalize(String content) {
-		if (content.isEmpty()) {
-			return content;
-		} else if (content.length() == 1) {
-			return content.toUpperCase();
-		} else {
-			return content.substring(0, 1).toUpperCase() + content.substring(1);
+
+	fun capitalize(content: String): String {
+		return when {
+			content.isEmpty() -> {
+				content
+			}
+			content.length == 1 -> {
+				content.toUpperCase()
+			}
+			else -> {
+				content.substring(0, 1).toUpperCase() + content.substring(1)
+			}
 		}
 	}
-	
-	public static String cameCaseToSpaces(String content) {
-		return content.replaceAll("([A-Z])", " $1").trim();
+
+	fun cameCaseToSpaces(content: String): String {
+		return content.replace("([A-Z])".toRegex(), " $1").trim { it <= ' ' }
 	}
-	
+
+	val TOKEN_REGEX = "\"[^\"]+\"|'[^']+'|\\S+"
+
+	fun tokenize(content: String, removeQuotes: Boolean = true): List<String> {
+
+		val regex =
+			Regex(TOKEN_REGEX, setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
+
+		val tokens = regex.findAll(content).map { it.value }.toList()
+
+		return if (removeQuotes)
+			tokens.map { it.replace(Regex("^['\"](.*)['\"]$"), "$1") }
+		else
+			tokens
+
+	}
+
 }
