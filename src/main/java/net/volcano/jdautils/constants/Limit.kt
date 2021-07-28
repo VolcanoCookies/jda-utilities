0333a8a798af8d417c2bb568c4cad5fa5151039f
Francis Gniady <franek.kg@gmail.com>
2021-07-28 18:58:27 +0200

    Moved to new limit fime

---
+++ src/main/java/net/volcano/jdautils/constants/Limit.kt
@@ -0,0 +1,41 @@
+package net.volcano.jdautils.constants
+
+const val SELECTION_OPTION_LABEL_LIMIT: Int = 25
+const val SELECTION_OPTION_VALUE_LIMIT: Int = 100
+const val SELECTION_OPTION_DESCRIPTION_LIMIT: Int = 50
+const val SELECTION_OPTION_LIMIT: Int = 25
+
+const val EMBED_TITLE_LIMIT: Int = 256
+const val EMBED_FIELD_TITLE_LIMIT: Int = 256
+
+/**
+ * Max text length of field
+ */
+const val EMBED_FIELD_VALUE_LIMIT: Int = 1024
+
+/**
+ * Max allowed fields in an embed
+ */
+const val EMBED_FIELD_COUNT_LIMIT: Int = 25
+
+/**
+ * Max length of embed footer text
+ */
+const val EMBED_FOOTER_LIMIT: Int = 2048
+
+/**
+ * Max length of embed author name
+ */
+const val EMBED_AUTHOR_NAME_LIMIT: Int = 256
+
+/**
+ * Max total length of embed
+ */
+const val EMBED_TOTAL_LIMIT: Int = 6000
+
+/**
+ * Max length of a embed description
+ */
+const val EMBED_DESCRIPTION_LIMIT: Int = 4096
+
+const val MESSAGE_EMBED_LIMIT: Int = 10
