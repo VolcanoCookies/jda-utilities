3d2cb6e781b94d535f415743144df361af8ee985
VolcanoCookies <franek.kg@gmail.com>
2021-07-28 22:12:53 +0200

    Add asString to map type

--- src/main/java/net/volcano/jdautils/utils/ListUtil.kt
+++ src/main/java/net/volcano/jdautils/utils/ListUtil.kt
@@ -1,17 +1,17 @@
 package net.volcano.jdautils.utils
 
-fun <T> Iterable<T>.asString(separator: String, maxLen: Int, func: (T) -> String) :List<String> {
+fun <T> Iterable<T>.asString(separator: String, maxLen: Int, func: (T) -> String): List<String> {
 	if (this.count() == 0)
 		return emptyList()
 	val output = mutableListOf<String>()
 	var string = ""
 	for (t in this) {
 		val s = func.invoke(t)
-		if ( string.length + s.length + separator.length > maxLen) {
+		if (string.length + s.length + separator.length > maxLen) {
 			output += string
 			string = ""
 		}
-		if ( string.isNotEmpty())
+		if (string.isNotEmpty())
 			string += separator
 		string += s
 	}
@@ -19,6 +19,14 @@ fun <T> Iterable<T>.asString(separator: String, maxLen: Int, func: (T) -> String
 	return output
 }
 
-fun <T> Iterable<T>.asString(separator: String, func: (T) -> String) :String {
+fun <T> Iterable<T>.asString(separator: String, func: (T) -> String): String {
 	return this.joinToString(separator = separator, transform = func)
+}
+
+fun <K, V> Map<K, V>.asString(separator: String, maxLen: Int, func: (Map.Entry<K, V>) -> String): List<String> {
+	return this.entries.asString(separator, maxLen, func)
+}
+
+fun <K, V> Map<K, V>.asString(separator: String, func: (Map.Entry<K, V>) -> String): String {
+	return this.entries.asString(separator, func)
 }
