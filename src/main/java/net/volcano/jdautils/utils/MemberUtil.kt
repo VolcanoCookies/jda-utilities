66a3f1783aabe263e76ad32aa32a9846e9ed839a
VolcanoCookies <franek.kg@gmail.com>
2021-08-26 23:08:24 +0200

    Fix sorting empty lists

--- src/main/java/net/volcano/jdautils/utils/MemberUtil.kt
+++ src/main/java/net/volcano/jdautils/utils/MemberUtil.kt
@@ -2,12 +2,15 @@ package net.volcano.jdautils.utils
 
 import net.dv8tion.jda.api.entities.Member
 
-fun Iterable<Member>.sortToMemberList() : Iterable<Member> {
-	return this.sortedWith(
-		compareBy({ it.roles.first { role -> role.isHoisted }?.position ?: -1 }, { it.effectiveName })
-	)
+fun Iterable<Member>.sortToMemberList(): Iterable<Member> {
+	return if (!this.iterator().hasNext()) {
+		this
+	} else
+		this.sortedWith(
+			compareBy({ it.roles.first { role -> role.isHoisted }?.position ?: -1 }, { it.effectiveName })
+		)
 }
 
-fun Member.format() : String {
+fun Member.format(): String {
 	return "${this.effectiveName} [${this.id}]"
 }
